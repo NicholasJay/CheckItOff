48610e75c7a1c29d4ee5317e8d03a6829ad7e98d
Nicholas Bundy <nicholasjaynovak@gmail.com>
2014-03-16 22:48:34 -0400

    Checklist deletes without refresh

--- app/assets/javascripts/checklists.js
+++ app/assets/javascripts/checklists.js
@@ -1,17 +1,3 @@
-
-function addItemToDatabase(entry){
-  $.post('/checklist', {tasks: entry, is_checked: "false"});
-  var item = $("<li>" + entry + "</li>");
-  var check = $("<input type='checkbox'/>");
-  check.id = item.id;
-  $(check).appendTo(item);
-  $("<span>X</span>").appendTo(item);
-  $(item).appendTo("ul");
-  $(check).on("change", strike);
-  $("span").on("click", deleteItem);
-  $("input").val('');
-}
-
 $("button").on("click", function(e){
   e.preventDefault();
   var entry = $("input").val();
@@ -19,7 +5,6 @@ $("button").on("click", function(e){
 });
 
 $("input:checkbox").on("change", strike);
-
 
 function strike(e){
   e.preventDefault();
@@ -43,9 +28,19 @@ function strike(e){
 
 function deleteItem(e){
   e.preventDefault();
-  var number = this.id;
-  $.ajax({
-    url: '/checklist/' + number,
-    type: 'delete'
-  });
+  this.parentElement.remove();
+}
+
+function addItemToDatabase(entry){
+  var itemDatabase = $.post('/checklist', {tasks: entry, is_checked: "false"});
+  var item = $("<li>" + entry + "</li>");
+  var check = $("<input type='checkbox'/>");
+  $(check).appendTo(item);
+  var span = $("<span>X</span>").appendTo(item);
+  check.id = itemDatabase.id;
+  span.id = itemDatabase.id;
+  $(item).appendTo("ul");
+  $(check).on("change", strike);
+  $("span").on("click", deleteItem);
+  $("input").val('');
 }
